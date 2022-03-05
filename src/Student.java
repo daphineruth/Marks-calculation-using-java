55c340b64d8aad0f1800b1c31e52b89d9dbdaf98
sheiswiz <daphneruth@304gmail.com>
2022-03-05 22:16:00 +0300

    added Event listener

--- src/Student.java
+++ src/Student.java
@@ -1,13 +1,32 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Student {
     private JButton calcButton;
-    private JTextField textStudent;
     private JTextField textMarks1;
     private JTextField textMarks2;
     private JTextField textMarks3;
     private JTextField textTotal;
     private JTextField textAvg;
-    private JTextField textField7;
     private JTextField textGrade;
+    private JPanel Main;
+    private JTextField textField1;
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Student");
+        frame.setContentPane(new Student().Main);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    public Student() {
+        calcButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
 }
