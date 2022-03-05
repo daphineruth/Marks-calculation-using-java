f93122b0a94d8145ec7cb4b8fb748eaa5058296f
sheiswiz <daphneruth@304gmail.com>
2022-03-05 21:44:05 +0300

    student marks calcuation using Java

---
+++ src/Student.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class Student {
+    private JButton calcButton;
+    private JTextField textStudent;
+    private JTextField textMarks1;
+    private JTextField textMarks2;
+    private JTextField textMarks3;
+    private JTextField textTotal;
+    private JTextField textAvg;
+    private JTextField textField7;
+    private JTextField textGrade;
+}
